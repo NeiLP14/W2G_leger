2c55b7d7537094d0dcaf6094fe990d3150b33533
Nei <lorenzo.helbert@iia-formation.fr>
2026-02-19 18:57:47 +0100

    Création de l'admin

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 #[ORM\InheritanceType('JOINED')]
 #[ORM\DiscriminatorColumn(name: 'discr', type: 'string')]
-#[ORM\DiscriminatorMap(['customer' => Customer::class])]
+#[ORM\DiscriminatorMap(['customer' => Customer::class, 'admin' => Admin::class])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
