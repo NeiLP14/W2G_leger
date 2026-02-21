03d475465e9db2124062760f4182a3d5b87d7543
Nei <lorenzo.helbert@iia-formation.fr>
2026-02-21 20:17:53 +0100

    card offres page accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\OffreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(OffreRepository $offreRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $offres = $offreRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            'offres' => $offres,
+        ]);
     }
 }
