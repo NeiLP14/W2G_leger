54a0aa78f75effffb1c7a2618fee6cf77b298852
Nei <lorenzo.helbert@iia-formation.fr>
2026-02-22 16:48:53 +0100

    page edit profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,8 +6,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
@@ -16,6 +20,52 @@ class UserController extends AbstractController
     {
         return $this->render('user/profile.html.twig', [
             'user' => $user,
+        ]);
+    }
+
+    #[Route('/profil/{id}/edit', name: 'user_profile_edit')]
+    public function edit(
+        User $user,
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $em
+    ): Response
+    {
+        // Sécurité : seul le propriétaire
+        if ($this->getUser() !== $user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        if ($request->isMethod('POST')) {
+
+            $user->setUsername($request->request->get('username'));
+            $user->setNom($request->request->get('nom'));
+            $user->setPrenom($request->request->get('prenom'));
+
+            $oldPassword = $request->request->get('old_password');
+            $newPassword = $request->request->get('new_password');
+
+            if ($newPassword) {
+                if (!$hasher->isPasswordValid($user, $oldPassword)) {
+                    $this->addFlash('error', 'Ancien mot de passe incorrect.');
+                } else {
+                    $user->setPassword(
+                        $hasher->hashPassword($user, $newPassword)
+                    );
+                }
+            }
+
+            $em->flush();
+
+            $this->addFlash('success', 'Profil mis à jour.');
+
+            return $this->redirectToRoute('user_profile', [
+                'id' => $user->getId()
+            ]);
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user
         ]);
     }
 }
