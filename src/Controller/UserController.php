e647740da32076b00c74dd7c732df0bde912e7a6
Nei <lorenzo.helbert@iia-formation.fr>
2026-02-24 11:34:16 +0100

    unit par user début

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
