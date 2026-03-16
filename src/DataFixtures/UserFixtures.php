cc5feb8b8311e9b8e8d519bc462cb93c016ad30b
NeiLP14 <lorenzo.helbert@iia-formation.fr>
2026-03-16 09:36:14 +0100

    new fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,28 +2,61 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Customer;
-use App\Entity\User;
+use App\Entity\Admin;
+use App\Entity\Accountant;
+use App\Entity\Technician;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
-    public function __construct(private UserPasswordHasherInterface $hasher)
-    {}
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
 
     public function load(ObjectManager $manager): void
     {
-        $customer1 = new Customer();
-        $customer1->setUsername('customer1');
-        $customer1->setEmail('customer1@gmail.com');
 
-        $password = $this->hasher->hashPassword($customer1, 'customer1');
-        $customer1->setPassword($password);
-        $customer1->setRoles(['ROLE_CUSTOMER']);
+        // ADMIN
+        $admin = new Admin();
+        $admin->setEmail('admin@worktogether.com');
+        $admin->setUsername('admin');
+        $admin->setRoles(['ROLE_ADMIN']);
 
-        $manager->persist($customer1);
+        $admin->setPassword(
+            $this->hasher->hashPassword($admin, 'admin')
+        );
+
+        $manager->persist($admin);
+
+
+        // ACCOUNTANT
+        $accountant = new Accountant();
+        $accountant->setEmail('accountant@worktogether.com');
+        $accountant->setUsername('accountant');
+        $accountant->setRoles(['ROLE_ACCOUNTANT']);
+
+        $accountant->setPassword(
+            $this->hasher->hashPassword($accountant, 'accountant')
+        );
+
+        $manager->persist($accountant);
+
+
+        // TECHNICIAN
+        $technician = new Technician();
+        $technician->setEmail('tech@worktogether.com');
+        $technician->setUsername('technician');
+        $technician->setRoles(['ROLE_TECHNICIAN']);
+
+        $technician->setPassword(
+            $this->hasher->hashPassword($technician, 'technician')
+        );
+
+        $manager->persist($technician);
+
+
         $manager->flush();
     }
 }
